3519a59b7978fba7bc117f273d15029e6bdaed5f
Irshad1404 <104862973+Irshad1404@users.noreply.github.com>
2022-05-08 01:52:57 +0100

    Update App-test.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -12,3 +12,9 @@ import renderer from 'react-test-renderer';
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+
+git add -A
+git config user.email "<your_email_address>"
+git config user.name "<your_name>"
+git commit -m 'Create Patch File'
+git format-patch -1 HEAD
